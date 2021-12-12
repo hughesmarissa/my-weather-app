8091736976f2359d4539b2d06175b5c6d0c70a53
Marissa Hughes <hughes.marissa331@gmail.com>
2021-12-12 14:15:13 -0800

    Add weateher api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const api = {
+    key: 'f380b5a69a1ca9ff2f4843a8ecd7bf3d',
+    base: "https://api.openweathermap.org/data/2.5/"
+};
+
 function App() {
   return (
     <div className="App">
